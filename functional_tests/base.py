c34551a6e5ddb57ad268c91b66873d838c30c8bf
Michal Pobrotyn <michal.pobrotyn@gmail.com>
2019-09-25 13:54:45 +0200

    managing the test database on staging done with fabric

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,3 +1,4 @@
+from .server_tools import reset_database
 import os
 import time
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
@@ -7,6 +8,19 @@ from selenium.common.exceptions import WebDriverException
 MAX_WAIT = 10
 
 
+def wait(fn):
+    def modified_fn(*args, **kwargs):
+        start_time = time.time()
+        while True:
+            try:
+                return fn(*args, **kwargs)
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+    return modified_fn
+
+
 class FunctionalTest(StaticLiveServerTestCase):
 
     def setUp(self):
@@ -14,22 +28,10 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.staging_server = os.environ.get('STAGING_SERVER')
         if self.staging_server:
             self.live_server_url = 'http://' + self.staging_server
+            reset_database(self.staging_server)
 
     def tearDown(self):
         self.browser.quit()
-
-    # pylint: disable=no-self-argument
-    def wait(fn):
-        def modified_fn(*args, **kwargs):
-            start_time = time.time()
-            while True:
-                try:
-                    return fn(*args, **kwargs)
-                except (AssertionError, WebDriverException) as e:
-                    if time.time() - start_time > MAX_WAIT:
-                        raise e
-                    time.sleep(0.5)
-        return modified_fn
 
     @wait
     def wait_for_row_in_list_table(self, row_text):
